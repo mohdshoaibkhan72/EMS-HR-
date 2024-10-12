9dcbd4eb8fccf2eed57ea3defee8d1f9d05da7b6
mohdshoaibkhan <mohdshoaibkhan7211@gmail.com>
2024-10-12 06:57:06 +0530

    testinf and some chnages in css

--- Clients/src/components/sidebar.jsx
+++ Clients/src/components/sidebar.jsx
@@ -3,31 +3,39 @@ import { NavLink } from "react-router-dom";
 import Admin from "./Navigation/Admin";
 import Leader from "./Navigation/Leader";
 import Employee from "./Navigation/Employee";
+import "./SideBar.css"; // Import the CSS file
 
 const SideBar = () => {
   const { user } = useSelector((state) => state.authSlice);
 
   return (
     <div className="main-sidebar">
+      <div className="navbarlogo">
+        <img
+          src="https://www.paisa4you.com/Images/png%20logo.png"
+          alt="Company Logo"
+          width="300"
+        />
+      </div>
       <aside id="sidebar-wrapper">
-        <div className="sidebar-brand">
-          <NavLink to="/home">Target Management</NavLink>
-        </div>
         <div className="sidebar-brand sidebar-brand-sm">
           <NavLink to="/home">TM</NavLink>
         </div>
-        {user.type === "Admin" ? (
-          <Admin />
-        ) : user.type === "Leader" ? (
-          <Leader />
-        ) : (
-          <Employee />
-        )}
+        <div className="sidebar-menu">
+          {user.type === "Admin" ? (
+            <Admin />
+          ) : user.type === "Leader" ? (
+            <Leader />
+          ) : (
+            <Employee />
+          )}
+        </div>
         <div className="mt-4 mb-4 p-3 hide-sidebar-mini">
           <a
             href="https://mohammad-shoaib-khan.vercel.app/"
             target="_blank"
             className="btn btn-primary btn-lg btn-block btn-icon-split"
+            rel="noreferrer"
           >
             <i className="fas fa-rocket"></i> Mohammad
           </a>
